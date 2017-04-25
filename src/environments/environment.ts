caf692251b88fe1edd6f299f6f868f40bee29c52
Roshan <roshanguptamca@gmail.com>
2017-04-25 15:29:09 +0200

    reverted

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   //api_url: 'http://127.0.0.1  :8000',
-  //api_url: 'https://api.tst.kpn.com'
-  api_url: 'https://api.tst.kpnnl.local'
+  api_url: 'https://api.tst.kpn.com'
+  //api_url: 'https://api.tst.kpnnl.local'
 
 };
