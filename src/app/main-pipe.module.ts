db99670b5dcba28d58a2c57d845c45b667a2096b
Roshan <roshanguptamca@gmail.com>
2017-05-02 11:29:00 +0200

    feedback fixing

--- src/app/main-pipe.module.ts
+++ src/app/main-pipe.module.ts
@@ -5,11 +5,12 @@ import { OrderBy } from './commons/pipes/orderby.pipe';
 import { SearchPipe } from './commons/pipes/search.pipe';
 import { ValueByKeyPipe } from './commons/pipes//value-by-key.pipe';
 import { CustomDatePipe } from './commons/pipes/date.pipe';
+import { CapitalizePipe } from './commons/pipes/capitalize.pipe';
 
 @NgModule({
-  declarations:[SearchPipe,OrderBy,ValueByKeyPipe,CustomDatePipe],
+  declarations:[SearchPipe,OrderBy,ValueByKeyPipe,CustomDatePipe,CapitalizePipe],
   imports:[CommonModule],
-  exports:[SearchPipe,OrderBy,ValueByKeyPipe,CustomDatePipe]
+  exports:[SearchPipe,OrderBy,ValueByKeyPipe,CustomDatePipe,CapitalizePipe]
 })
 
 export class MainPipe{}
