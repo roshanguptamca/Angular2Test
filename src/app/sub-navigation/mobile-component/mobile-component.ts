47ab82111d9d6b3544646f67b0a00b6fad95c047
Roshan <roshanguptamca@gmail.com>
2017-05-08 10:54:32 +0200

    fixes as for Edit failure The following ruling must be applied by editingan failure: Only failures with the following state are allowed to be edited: (When not allowed hide the edit icon) PLANNED OPEN When an failure is deleted (status: STATE_CLOSED) it's not allowed to edit an failure anymore (hide the edit icon) The following fields are editable: End date Description Criteria (It's only allowed to add criteria, not to delete criteria) Delete failure The following ruling must be applied by deleting an failure: Only failures with the following state are allowed to be deleted: (When not allowed hide the trashcan icon) NEW PLANNED OPEN AWAITING_APPROVAL When an failure is deleted (status: STATE_CLOSED) there are no other option allowed (Edit, close) accept for 'Communication'

--- src/app/sub-navigation/mobile-component/mobile-component.ts
+++ src/app/sub-navigation/mobile-component/mobile-component.ts
@@ -356,7 +356,7 @@ onChangService(newvalue) {
     var isCloseIconDisplay = false;
    if (AppConstant.APP_ARCHIVED_FAILURE_MOBILE_URL === this.selectedUrl || AppConstant.APP_ARCHIVED_PLANNED_MAINTENCE_BORDBAND_URL === this.selectedUrl) {
         isCloseIconDisplay = false;
-    } else if(failureStatus === 'open' || failureStatus === 'state_awaiting_approval' || failureStatus === 'state_planned'){
+    } else if(failureStatus === 'open' || failureStatus === 'new' || failureStatus === 'planned' || failureStatus === 'awaiting'){
       isCloseIconDisplay = true;
     }
     else {
